97e8596d25691ec35f2c06f3e21d38dced79ac2a
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2024-03-26 23:16:59 +0530

    add config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#292019",
+        black: "#000000",
+        button: "#B91122",
+        buttonSecondary: "#FCC7C3",
+        textLight: "#C8CED8",
+      },
+      fontFamily: {
+        abhaya: ["Abhaya Libre", "serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+      content: {
+        dottedlines: "url('./assets/dotted-line-bg.svg')",
+        abstractwaves: "url('./assets/AbstractWaves.png')",
+        sparkles: "url('./assets/Sparkles.png')",
+        circles: "url('./assets/Circles.png')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
   },
   plugins: [],
 };
