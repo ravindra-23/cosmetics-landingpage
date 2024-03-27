2564b08a5ebf813102ce7d187e4cd0613f91fa77
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2024-03-27 21:42:22 +0530

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,54 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import Feature from "./components/feature";
+import Choose from "./components/choose";
+import Clients from "./components/clients";
+import Testimonials from "./components/testimonials";
+import Faqs from "./components/faqs";
+import CTA from "./components/cta";
+import Footer from "./components/footer";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [selectedPage, setSelectedPage] = useState("home");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage("home");
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <>
-      <h1 className="text-5xl font-bold underline">Hello World</h1>
-    </>
+    <div className="app bg-white">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+
+      <Hero />
+
+      <Feature />
+
+      <Choose />
+
+      <Clients />
+
+      <Testimonials />
+
+      <Faqs />
+
+      <CTA />
+
+      <Footer />
+    </div>
   );
 }
 
